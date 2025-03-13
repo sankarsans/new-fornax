dfe0bf5e987d417a7c53ffd0ce6faa0d748da0f1
[200~sankarsans~ <sssankarsam@gmail.com>
2025-03-13 16:06:11 +0530

    new

--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -1,29 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import  data from '../../shared/data.json';
+import data from '../../shared/data.json';
 import { ProjectInfo } from 'src/shared/data';
 import { ActivatedRoute } from '@angular/router';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
-  styleUrls: ['./project-details.component.scss']
+  styleUrls: ['./project-details.component.scss'],
 })
 export class ProjectDetailsComponent implements OnInit {
   projectInfo: ProjectInfo[] = data;
   activeItem!: ProjectInfo;
   activeId: string | null;
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    center: false,
+    dots: false,
+    autoHeight: true,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  };
 
   constructor(private route: ActivatedRoute) {
     this.activeId = this.route.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
-    const filterItem = this.projectInfo.filter(item=>{
+    const filterItem = this.projectInfo.filter((item) => {
       return item.id === this.activeId;
-    })
+    });
     this.activeItem = filterItem[0];
-    console.log(this.activeItem);
-
   }
-
 }
--- src/app/projects-list/projects-list.component.ts
+++ src/app/projects-list/projects-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import  data from '../../shared/data.json';
+import data from '../../shared/data.json';
 import { ProjectInfo } from 'src/shared/data';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -7,11 +7,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-projects-list',
   templateUrl: './projects-list.component.html',
-  styleUrls: ['./projects-list.component.scss']
+  styleUrls: ['./projects-list.component.scss'],
 })
 export class ProjectsListComponent implements OnInit {
   projectInfo: ProjectInfo[] = data;
-  Images: any;  
+  Images: any;
   customOptions: OwlOptions = {
     loop: true,
     autoplay: true,
@@ -28,23 +28,21 @@ export class ProjectsListComponent implements OnInit {
       },
       1000: {
         items: 2,
-      }
-    }
-  }  
+      },
+    },
+  };
   projectPage: boolean = false;
 
-  constructor(private route: Router) { 
+  constructor(private route: Router) {
     const currenturl = this.route.url;
-      console.log(currenturl);
-      if(currenturl.includes('projects')){
-        this.projectPage = true;
-      } else {
-        this.projectPage = false;
-      }
+    if (currenturl.includes('projects')) {
+      this.projectPage = true;
+    } else {
+      this.projectPage = false;
+    }
   }
 
   ngOnInit(): void {
     this.Images = ['assets/images/banner-1.png', 'assets/images/banner-2.png'];
   }
-
 }
